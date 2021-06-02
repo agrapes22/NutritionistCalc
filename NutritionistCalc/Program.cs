cdc90ab7a6a12e4e8bedf73c94c259c2b7999b98
agrapes22 <agrapes22@gmail.com>
2021-06-01 21:22:06 -0700

    Initial Commit

--- NutritionistCalc/Program.cs
+++ NutritionistCalc/Program.cs
@@ -10,12 +10,40 @@ namespace NutritionistCalc
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            //Welcome to the calculator
+            Console.WriteLine("Hello welcome to the Nutritionist calculator!");
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            //get grams of fat from user
+            Console.WriteLine("Please enter the number of grams of fat: ");
+            double FatGrams = double.Parse(Console.ReadLine());
+            //output to calories
+            Console.WriteLine(FatGrams + " grams of fat is " + FatCalories(FatGrams) + " calories.");
+
+            //get number of carbs
+            Console.WriteLine("Please enter the number of carbohydrates: ");
+            double CarbGrams = double.Parse(Console.ReadLine());
+            //output
+            Console.WriteLine(CarbGrams + " carbohydrates is " + CarbCalories(CarbGrams) + " calories.");
+
         }
+
+        //This method should accept a number of fat grams as an argument 
+        //and return the number of calories from that amount of fat.
+        //Calories from fat = Fat grams × 9
+        public static double FatCalories(double FatGrams)
+        {
+            double FatCal = FatGrams * 9;
+            return FatCal;
+        }
+
+        //This method should accept a number of carbohydrate grams as an argument and return the number of 
+        //calories from that amount of carbohydrates.
+        //Calories from carbs = Carbs grams × 4
+        public static double CarbCalories(double Carbs)
+        {
+            double CarbCal = Carbs * 4;
+            return CarbCal;
+        }
+
     }
 }
